f402fca79539830353bdca0167b9de4b5c7d543a
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-02 11:44:13 +0300

    folder scructure done

--- Gestures/ContentView.swift
+++ Gestures/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            Text("Magnification")
+            Text("Drag")
+            Text("Rotation")
+            
         }
         .padding()
     }
---
+++ Gestures/Suporting Views/NiceTextView.swift
@@ -0,0 +1,18 @@
+//
+//  NiceTextView.swift
+//  Gestures
+//
+//  Created by Ahmet Ozen on 2.02.2024.
+//
+
+import SwiftUI
+
+struct NiceTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NiceTextView()
+}
