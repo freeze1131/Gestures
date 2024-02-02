d20eb37885b6c3d4d143e18c9458f62903a4f665
freeze1131 <ozenahmetcemil@gmail.com>
2024-02-02 12:00:15 +0300

    added square row

---
+++ Gestures/Suporting Views/SquaresRow.swift
@@ -0,0 +1,34 @@
+//
+//  SquaresRow.swift
+//  Gestures
+//
+//  Created by Ahmet Ozen on 2.02.2024.
+//
+
+import SwiftUI
+
+struct SquaresRow: View {
+    let colors: [Color]
+    let dim: CGFloat
+    
+    init(colors: [Color], dim: CGFloat = 50) {
+        self.colors = colors
+        self.dim = dim
+    }
+    
+    var body: some View {
+        GridRow {
+            ForEach(colors,id:\.self) { color in
+                color
+                    .frame(width:dim,height: dim)
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+        }
+    }
+}
+
+#Preview {
+    Grid {
+        SquaresRow(colors: [.red,.blue,.green], dim: 50)
+    }
+}
